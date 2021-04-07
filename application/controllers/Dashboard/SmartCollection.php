78a2f752026df7c6181822053e107784cac249d9
Farhan En Muzaki <fahmuzaki@gmail.com>
2021-04-07 16:11:24 +0700

    Dashboard SmartCollection Perfomance (Model View dan Controller)

--- application/controllers/Dashboard/SmartCollection.php
+++ application/controllers/Dashboard/SmartCollection.php
@@ -35,6 +35,58 @@ class SmartCollection extends CI_Controller {
 		
 	}
 
+	public function perfomance()
+	{
+		if (isset($_POST['template']) ) {
+			$sel_template = $this->input->post('template');
+			$sel_date = $this->input->post('datena');
+		}
+		else{
+			$sel_template = 'daily';
+			$sel_date = date('Y-m-d');
+		}
+
+		$data = array(
+			'title_page_big'		=> 'Dashboard Performance Smart Collection',
+			'title'					=> $this->title,
+			'sel_date'				=> $sel_date,
+			'sel_template'			=> $sel_template,
+		);
+		$data['controller'] = $this;
+		// load query dan tampung ke data
+		$getDateData = $this->SmartCollectionModel->get_date_data($sel_date);
+		$l_date = date('Y-m-d', strtotime($getDateData[0]['date_value']));
+		switch ($template) {
+			case 'monthly':
+				$f_date = date('Y-m-01', strtotime($getDateData[0]['date_value']));
+				// $l_date = date('Y-m-t', strtotime($getDateData[0]['date_value']));
+				break;
+			case 'weekly':
+				$f_date = date('Y-m-d', mktime(0, 0, 0, $getDateData[0]['month_number'], $getDateData[0]['is_first_day_in_week'], $getDateData[0]['year_4']));
+				// $l_date = date('Y-m-d', strtotime($getDateData[0]['date_value']));
+				break;
+			case 'daily':
+				$f_date = date('Y-m-d', strtotime($getDateData[0]['date_value']));
+				// $l_date = date('Y-m-d', strtotime($getDateData[0]['date_value']));
+				break;
+			default:
+				$f_date = date('Y-m-d', strtotime($getDateData[0]['date_value']));
+				// $l_date = date('Y-m-d', strtotime($getDateData[0]['date_value']));
+				break;
+		}
+
+		$data['summary_order_by_date'] = $this->SmartCollectionModel->get_summary_order_by_date($f_date, $l_date);
+		$data['summary_unique_customer_by_date'] = $this->SmartCollectionModel->get_summary_unique_customer_by_date($f_date, $l_date);
+		$data['summary_payment_by_regional'] = $this->SmartCollectionModel->get_summary_payment_by_regional($f_date, $l_date);
+		// Perilakuan Khusus untuk Summary Order by Date Chart
+		if ($sel_template == 'daily') {
+			$f_date = date_format(date_sub(date_create($l_date),date_interval_create_from_date_string("30 days")), 'Y-m-d');
+		}
+		$data['summary_order_by_date_chart'] = $this->SmartCollectionModel->get_summary_order_by_date_chart($f_date, $l_date);
+
+		$this->load->view('Dashboard/SmartCollection_Perfomance', $data);
+	}
+
 }
 
 /* End of file smartCollection.php */
--- application/models/Dashboard/SmartCollectionModel.php
+++ application/models/Dashboard/SmartCollectionModel.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class SmartCollectionModel extends CI_Model {
 
+	// #Monitoring
 	// Query untuk Pie Chart Summary Order (Today)
 	public function get_summary_order_today($value='')
 	{
@@ -136,17 +137,92 @@ class SmartCollectionModel extends CI_Model {
 		return $result->result_array();
 	}
 
+	// #Perfomance
+	// Get Summary Order by Status berdasarkan Daterange
+	public function get_summary_order_by_date($f_date, $l_date)
+	{
+		$this->db->select('dim_status.status_value AS label
+			, COUNT(fact_interaction.status_key) AS status_count ', false);
+		$this->db->from('fact_interaction');
+		$this->db->join('dim_date', 'fact_interaction.date_key = dim_date.date_key');
+		$this->db->join('dim_status', 'fact_interaction.status_key = dim_status.status_key');
+		$this->db->where('DATE(dim_date.date_value) BETWEEN CAST("'.$f_date.'" AS DATE) AND CAST("'.$l_date.'" AS DATE)');
+		$this->db->group_by('fact_interaction.status_key');
+		$this->db->order_by('fact_interaction.status_key');
+		$result = $this->db->get();
+
+		return $result->result_array();
+	}
+
+	// Get Unique Customer berdasarkan Daterange
+	// Untuk Unique Customer nya perlu di tambahin quqery lagi (?) 
+	public function get_summary_unique_customer_by_date($f_date, $l_date)
+	{
+		$this->db->select('COUNT(dim_bayar.customer_key) AS count_customer
+			, SUM(CASE WHEN dim_bayar.date_key IS NOT NULL THEN 1 ELSE 0 END) AS count_pay
+			, SUM(CASE WHEN dim_bayar.date_key IS NOT NULL THEN 0 ELSE 1 END) AS count_nopay
+			, SUM(dim_bayar.bayar_value) AS sum_payment', false);
+		$this->db->from('dim_bayar');
+		$this->db->join('dim_periode', 'dim_periode.periode_key = dim_bayar.periode_key');
+		$this->db->join('dim_date', 'dim_periode.date_key = dim_date.date_key');
+		$this->db->where('DATE(dim_date.date_value) BETWEEN CAST("'.$f_date.'" AS DATE) AND CAST("'.$l_date.'" AS DATE)');
+		$result = $this->db->get();
+
+		return $result->result_array();
+	}
+
+	// Get Summary Order untuk Barchart + LineAxis, dg hasil order(all fact interaction), success dan yang lakukan payment
+	public function get_summary_order_by_date_chart($f_date, $l_date)
+	{
+		// Anggap status success == 1
+		$this->db->select('dim_date.date_value AS label_date
+			, COUNT(*) AS count_order
+			, SUM(CASE WHEN fact_interaction.status_key = 1 THEN 1 ELSE 0 END) AS count_success
+			, COUNT(dim_bayar.date_key) AS count_pay', false);
+		$this->db->from('fact_interaction');
+		$this->db->join('dim_date', 'fact_interaction.date_key = dim_date.date_key');
+		$this->db->join('dim_bayar', 'fact_interaction.bayar_key = dim_bayar.bayar_key', 'left');
+		$this->db->where('DATE(dim_date.date_value) BETWEEN CAST("'.$f_date.'" AS DATE) AND CAST("'.$l_date.'" AS DATE)');
+		$this->db->group_by('fact_interaction.date_key');
+		$this->db->order_by('fact_interaction.date_key');
+		$result = $this->db->get();
+
+		return $result->result_array();
+	}
+
+	// Get Summary Payemnt by Regional
+	public function get_summary_payment_by_regional($f_date, $l_date)
+	{
+		// Anggap success key = 1
+		// untuk hitung count customer masih agak bermasalah sih karena harus hitung parameter yg mana
+		$this->db->select('dim_regional.regional_value AS label_regional
+			, COUNT(*) AS count_order
+			, SUM(CASE WHEN fact_interaction.status_key = 1 THEN 1 ELSE 0 END) AS count_success
+			, COUNT(fact_interaction.customer_key) AS count_customer 
+			, SUM(IFNULL(dim_bayar.bayar_value, 0)) AS sum_payment', false);
+		$this->db->from('fact_interaction');
+		$this->db->join('dim_date ', 'fact_interaction.date_key = dim_date.date_key');
+		$this->db->join('dim_regional', 'fact_interaction.regional_key = dim_regional.regional_key');
+		$this->db->join('dim_bayar', 'fact_interaction.bayar_key = dim_bayar.bayar_key', 'left');
+		$this->db->where('DATE(dim_date.date_value) BETWEEN CAST("'.$f_date.'" AS DATE) AND CAST("'.$l_date.'" AS DATE)');
+		$this->db->group_by('fact_interaction.regional_key');
+		$this->db->order_by('fact_interaction.regional_key');
+		$result = $this->db->get();
+
+		return $result->result_array();
+	}
+
+	// Utilities Model
 	// Query untuk nyari data tanggal
 	public function get_date_data($f_date)
 	{
-		$this->db->select('date_value');
+		// $this->db->select('date_value');
 		$this->db->from('dim_date');
 		$this->db->where('DATE(date_value) = CAST("'.$f_date.'" AS DATE)');
 		$result = $this->db->get();
 
 		return $result->result_array();
 	}
-
 }
 
 /* End of file smartCollectionModel.php */
