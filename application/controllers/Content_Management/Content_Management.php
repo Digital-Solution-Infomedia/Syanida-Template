a0c817dd9eb44738dac2b7fd89cf0835cc59471c
Aryo Fajar <aryoff@gmail.com>
2021-04-21 15:48:14 +0700

    main flow, done

--- application/controllers/Content_Management/Content_Management.php
+++ application/controllers/Content_Management/Content_Management.php
@@ -1,7 +1,9 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+const NOACCESS = 'No direct script access allowed';
+defined('BASEPATH') || exit(NOACCESS);
 
-class Content_Management extends CI_Controller {
+class Content_Management extends CI_Controller
+{
 
 	public function __construct()
 	{
@@ -16,176 +18,33 @@ class Content_Management extends CI_Controller {
 		$this->template->load('Content_management/index2');
 	}
 
-	// Save Generator Data
-	// Save Generator Email
-	public function save_email_template()
-	{
-		/*var_dump ($this->input->post('nama_campaign'));
-		var_dump ($this->input->post('content'));
-		var_dump ($this->input->post('campaign_image'));
-		var_dump ($this->input->post('button_for_link'));
-		die();*/
-		// Pasang Form Validation
-		$this->load->helper(array('form', 'url'));
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules('nama_campaign', 'Nama Campaign', 'required');
-		$this->form_validation->set_rules('content', 'Content', 'required');
-		// Check kalo files dikirim ga, kalo ga dikirim bikin rules agar ada report errornya
-		if (empty($_FILES['campaign_image']['name']))
-		{
-			$this->form_validation->set_rules('campaign_image', 'Campaign Image', 'required');
-		}
-
-		$this->form_validation->set_rules('button_for_link', 'Link for Button', 'required');
-
-		// Check Validation dari Form
-		if ($this->form_validation->run() == FALSE)
-		{
-			// Kembalikan ke View Index
-			echo validation_errors('<span class="error">', '</span>');
-			// Harusnya kasih variabel Error disini
-			redirect('/Content_Management/Content_Management/index#error_on_form', 'refresh');
-		}
-		else
-		{			
-			// Upload Gambar Campaign
-			// Ambil file name dan simpan di variabel
-			$new_name = time().'email_'.$this->input->post('nama_campaign').'_'.$_FILES["campaign_image"]['name'];
-			// File Path
-			$file_path = 'uploads/campaign_image/';
-			// Load library Upload
-			$config['upload_path'] = './'.$file_path;
-			$config['allowed_types'] = 'jpg';
-			$config['file_name'] = $new_name;
-			$this->load->library('upload', $config);
-			if ( ! $this->upload->do_upload('campaign_image'))
-			{
-				$error = array('error' => $this->upload->display_errors());
-				print_r($error);
-
-				// $this->load->view('upload_form', $error);
-				redirect('/Content_Management/Content_Management/index#error_on_uploading_image', 'refresh');
-			}
-
-			$template_content[] = ['content' => $this->input->post('content')];
-			$template_content[] = ['campaign_image' => $file_path.$new_name.'.jpg'];
-			$template_content[] = ['button_for_link' => $this->input->post('button_for_link')];
-			// Bungkus sebagai JSON untuk Template Content
-			// panggil fungsi untuk save data
-			$this->ContentManagementModel->save_template('email', $this->input->post('nama_campaign'), json_encode($template_content));
-			redirect('/Content_Management/Content_Management/index#success', 'refresh');
-		}
-	}
-
-	// Save Generator Whatsapp
-	public function save_whatsapp_template()
-	{
-		/*var_dump ($this->input->post('nama_campaign'));
-		var_dump ($this->input->post('content'));
-		var_dump ($this->input->post('campaign_image'));
-		die();*/
-		// Pasang Form Validation
-		$this->load->helper(array('form', 'url'));
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules('nama_campaign', 'Nama Campaign', 'required');
-		$this->form_validation->set_rules('content', 'Content', 'required');
-		// Check kalo files dikirim ga, kalo ga dikirim bikin rules agar ada report errornya
-		if (empty($_FILES['campaign_image']['name']))
-		{
-			$this->form_validation->set_rules('campaign_image', 'Campaign Image', 'required');
-		}
-
-
-		// Check Validation dari Form
-		if ($this->form_validation->run() == FALSE)
-		{
-			// Kembalikan ke View Index
-			echo validation_errors('<span class="error">', '</span>');
-			// Harusnya kasih variabel Error disini
-			redirect('/Content_Management/Content_Management/index#error_on_form', 'refresh');
-		}
-		else
-		{			
-			// Upload Gambar Campaign
-			// Ambil file name dan simpan di variabel
-			$new_name = time().'whatsapp_'.$this->input->post('nama_campaign').'_'.$_FILES["campaign_image"]['name'];
-			// File Path
-			$file_path = 'uploads/campaign_image/';
-			// Load library Upload
-			$config['upload_path'] = './'.$file_path;
-			$config['allowed_types'] = 'jpg';
-			$config['file_name'] = $new_name;
-			$this->load->library('upload', $config);
-			if ( ! $this->upload->do_upload('campaign_image'))
-			{
-				$error = array('error' => $this->upload->display_errors());
-				print_r($error);
-
-				// $this->load->view('upload_form', $error);
-				redirect('/Content_Management/Content_Management/index#error_on_uploading_image', 'refresh');
-			}
-
-			$template_content[] = ['content' => $this->input->post('content')];
-			$template_content[] = ['campaign_image' => $file_path.$new_name.'.jpg'];
-			// Bungkus sebagai JSON untuk Template Content
-			// panggil fungsi untuk save data
-			$this->ContentManagementModel->save_template('whatsapp', $this->input->post('nama_campaign'), json_encode($template_content));
-			redirect('/Content_Management/Content_Management/index#success', 'refresh');
-		}
-	}
-
-	// Save Generator SMS
-	public function save_sms_template()
-	{
-		/*var_dump ($this->input->post('nama_campaign'));
-		var_dump ($this->input->post('content'));
-		die();*/
-		// Pasang Form Validation
-		$this->load->helper(array('form', 'url'));
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules('nama_campaign', 'Nama Campaign', 'required');
-		$this->form_validation->set_rules('content', 'Content', 'required');
-
-		// Check Validation dari Form
-		if ($this->form_validation->run() == FALSE)
-		{
-			// Kembalikan ke View Index
-			// Harusnya kasih variabel Error disini
-			redirect('/Content_Management/Content_Management/index#error_on_form', 'refresh');
-		}
-		else
-		{
-			$template_content[] = ['content' => $this->input->post('content')];
-			// Bungkus sebagai JSON untuk Template Content
-			// panggil fungsi untuk save data
-			$this->ContentManagementModel->save_template('sms', $this->input->post('nama_campaign'), json_encode($template_content));
-			redirect('/Content_Management/Content_Management/index#success', 'refresh');
-		}
-	}
-
 	public function blast_off() //**** UNTUK DI CRONJOB ****/
 	{
 		$data = $this->ContentManagementModel->get_blast_data();
-		foreach ($data as $row)
-		{
+		foreach ($data as $row) {
 			$success = false;
 			// echo $row->cust_id;
 			// echo $row->media;
 			// echo $row->content_id;
 			// echo $row->unique_link;
 			// echo $row->target;
-			switch ($row->media) {
+			switch ($row->media) { //TODO tambah sistem blast
 				case 'whatsapp':
 					# Blast WA
+					$content = $this->ContentManagementModel->unique_link_access('whatsapp', $row->unique_link);
 					$success = true;
 					break;
 				case 'sms':
 					# Blast sms
+					$content = $this->ContentManagementModel->unique_link_access('sms', $row->unique_link);
 					$success = true;
 					break;
 				case 'email':
 					# Blast email
+					$content = $this->ContentManagementModel->unique_link_access('email', $row->unique_link);
 					$success = true;
+					break;
+				default:
 					break;
 			}
 			if ($success) {
@@ -194,46 +53,77 @@ class Content_Management extends CI_Controller {
 		}
 	}
 
-	public function customer_responded()
+	public function saveContent()
 	{
-		$this->ContentManagementModel->clear_blast_link($unique_link);
+		if ($this->input->is_ajax_request()) {
+			$nama = $this->input->post('nama');
+			$sms_template = $this->input->post('sms_template');
+			$email_template = $this->input->post('email_template');
+			$landingpage_template = $this->input->post('landingpage_template');
+			$filter_parameter = $this->input->post('filter_parameter');
+			$tipe_content = $this->input->post('tipe_content');
+			$starttime = $this->input->post('starttime');
+			$endtime = $this->input->post('endtime');
+			$content_id = $this->ContentManagementModel->saveContent($nama, $sms_template, $email_template, $landingpage_template, $tipe_content, $starttime, $endtime);
+			$this->ContentManagementModel->insert_cust_blast($content_id, $filter_parameter); //generate blast data
+
+			// $hasil = array('response' => 'success', 'results' => $this->ContentManagementModel->getTemplateData($template_id));
+			// echo json_encode($hasil);
+		} else {
+			echo NOACCESS;
+		}
 	}
 
-	public function generate_landingpage()
+	public function getTypeTemplate()
 	{
-		$this->ContentManagementModel->unique_link_access($unique_link);
-	}
-
-	public function generate_blast_data()
-	{
-		$this->ContentManagementModel->insert_cust_blast();
-	}
-
-	public function getTypeTemplate() {
 		if ($this->input->is_ajax_request()) {
 			$hasil = array('response' => 'success', 'results' => $this->ContentManagementModel->getTypeTemplate());
 			echo json_encode($hasil);
 		} else {
-			echo "No direct script access allowed";
+			echo NOACCESS;
 		}
 	}
 
-	public function getTemplateData() {
+	public function getTemplateData()
+	{
 		if ($this->input->is_ajax_request()) {
 			$template_id = $this->input->post('template_id');
 			$hasil = array('response' => 'success', 'results' => $this->ContentManagementModel->getTemplateData($template_id));
 			echo json_encode($hasil);
 		} else {
-			echo "No direct script access allowed";
+			echo NOACCESS;
 		}
 	}
 
-	public function LandingPage() {
+	public function LandingPage()
+	{
 		$unique_link = $this->input->get('unique_link', TRUE);
-		$data['customized_view'] = $this->ContentManagementModel->unique_link_access($unique_link);
-		$this->template->load('Content_management/LandingPage',$data);
+		$data['customized_view'] = $this->ContentManagementModel->unique_link_access('landingpage', $unique_link);
+		// $this->ContentManagementModel->clear_blast_link($unique_link); //Clear unique link 
+		$this->template->load('Content_management/LandingPage', $data);
 	}
 
+	public function getFieldData()
+	{
+		if ($this->input->is_ajax_request()) {
+			$selected_field = $this->input->post('selectedField');
+			$hasil = array('response' => 'success', 'results' => '[{"id":"1","text":"Parameter 1"},{"id":"2","text":"Parameter 2"},{"id":"3","text":"Parameter 3"}]');
+			echo json_encode($hasil);
+		} else {
+			echo NOACCESS;
+		}
+	}
+
+	public function calculatePotentialTarget()
+	{
+		if ($this->input->is_ajax_request()) {
+			$filter_parameter = $this->input->post('filter_parameter');
+			$hasil = array('response' => 'success', 'results' => '');
+			echo json_encode($hasil);
+		} else {
+			echo NOACCESS;
+		}
+	}
 }
 
 /* End of file content_Management.php */
--- application/models/ContentManagementModel.php
+++ application/models/ContentManagementModel.php
@@ -1,16 +1,28 @@
 <?php
 defined('BASEPATH') || exit('No direct script access allowed');
 
-class ContentManagementModel extends CI_Model {
-
+class ContentManagementModel extends CI_Model
+{
 	public function save_template(string $jenis_template, string $campaign_name, string $template_content)
 	{
 		$this->db->insert('content_management', [
-			'template_type' => $jenis_template
-			, 'campaign_name' => $campaign_name
-			, 'template_content' => $template_content
+			'template_type' => $jenis_template, 'campaign_name' => $campaign_name, 'template_content' => $template_content
 		]);
 		return true;
+	}
+	public function saveContent($nama, $sms_template, $email_template, $landingpage_template, $tipe_content, $starttime, $endtime = null)
+	{
+		$design_data = array('sms' => $sms_template, 'email' => $email_template, 'landingpage' => $landingpage_template);
+		$data = array(
+			'nama' => $nama,
+			'tipe_content' => $tipe_content,
+			'design_data' => json_encode($design_data),
+			'starttime' => $starttime,
+			'endtime' => $endtime,
+			'active' => '0'
+		);
+		$this->db->insert('content_campaign', $data);  //TODO gimana kalo bukan tambah baru, tapi update
+		return $this->db->insert_id();
 	}
 	public function get_blast_data() // get data yg mau di blast dari DB
 	{
@@ -22,22 +34,22 @@ class ContentManagementModel extends CI_Model {
 	}
 	public function update_blast_result(string $unique_link) //Bilamana blast sukses dieksekusi (bukan blast agree)
 	{
-		return $this->db->query("UPDATE content_customer_blast SET stage_flag = stage_flag + 1 WHERE unique_link = ".$unique_link.";");
+		return $this->db->query("UPDATE content_customer_blast SET stage_flag = stage_flag + 1 WHERE unique_link = " . $unique_link . ";");
 	}
 	public function clear_blast_link(string $unique_link) //Final Function, kalau pelanggan agree, disagree, tidak ada respon sama sekali, hentikan blast
 	{
-		return $this->db->query("UPDATE content_customer_blast SET unique_link = NULL WHERE unique_link = ".$unique_link.";");
+		return $this->db->query("UPDATE content_customer_blast SET unique_link = NULL WHERE unique_link = " . $unique_link . ";");
 	}
-	public function unique_link_access(string $unique_link) //Ambil data saat customer click unique link
+	public function unique_link_access(string $media, string $unique_link) //Ambil data berdasarkan unique link
 	{
-		return $this->db->query('SELECT JSON_UNQUOTE(JSON_EXTRACT(design_data, "$.landingpage")) AS landingpage FROM content_campaign WHERE id=(SELECT content_id FROM content_customer_blast WHERE unique_link = "'.$unique_link.'")')->result()[0]->landingpage;
+		return $this->db->query('SELECT JSON_UNQUOTE(JSON_EXTRACT(design_data, "$.' . $media . '")) AS ' . $media . ' FROM content_campaign WHERE id=(SELECT content_id FROM content_customer_blast WHERE unique_link = "' . $unique_link . '")')->result()[0]->$media;
 	}
 	public function insert_cust_blast(integer $content_id, array $parameter)
 	{
-		$sql = 'INSERT INTO content_customer_blast (stage_data,unique_link,content_id) SELECT CONCAT("[",IF(email IS NOT NULL,CONCAT(\'{"media":"email","target":"\',email,\'","schedule":\',UNIX_TIMESTAMP(DATE_ADD(NOW(), INTERVAL 0 DAY)),\'}\'),""),IF(LENGTH(SUBSTRING(ani,LOCATE("8",ani),LENGTH(ani)-LOCATE("8",ani)+1))>10 , CONCAT(IF(email IS NOT NULL,",",""),\'{"media":"sms","target":"\',ani,\'","schedule":\',UNIX_TIMESTAMP(DATE_ADD(NOW(), INTERVAL 3 DAY)),\'},{"media":"whatsapp","target":"\',ani,\'","schedule":\',UNIX_TIMESTAMP(DATE_ADD(NOW(), INTERVAL 6 DAY)),\'}\'), ""),"]") AS stage_data, SHA2(CONCAT(cust_id,'.$content_id.'), 512) AS unique_link,'.$content_id.' AS content_id FROM (;';
+		$sql = 'INSERT INTO content_customer_blast (stage_data,unique_link,content_id) SELECT CONCAT("[",IF(email IS NOT NULL,CONCAT(\'{"media":"email","target":"\',email,\'","schedule":\',UNIX_TIMESTAMP(DATE_ADD(NOW(), INTERVAL 0 DAY)),\'}\'),""),IF(LENGTH(SUBSTRING(ani,LOCATE("8",ani),LENGTH(ani)-LOCATE("8",ani)+1))>10 , CONCAT(IF(email IS NOT NULL,",",""),\'{"media":"sms","target":"\',ani,\'","schedule":\',UNIX_TIMESTAMP(DATE_ADD(NOW(), INTERVAL 3 DAY)),\'},{"media":"whatsapp","target":"\',ani,\'","schedule":\',UNIX_TIMESTAMP(DATE_ADD(NOW(), INTERVAL 6 DAY)),\'}\'), ""),"]") AS stage_data, SHA2(CONCAT(cust_id,' . $content_id . '), 512) AS unique_link,' . $content_id . ' AS content_id FROM (;';
 		$sql .= 'SELECT cust_id,ani_number,email FROM db_profiling WHERE true';
 		foreach ($parameter as $param) {
-			$sql .= " AND cust_id IN (SELECT cust_id FROM ".$param->database_name." WHERE ".$param->filter.")";
+			$sql .= " AND cust_id IN (SELECT cust_id FROM " . $param->database_name . " WHERE " . $param->filter . ")";
 		}
 		$sql .= ') temp';
 		$this->db->query($sql);
@@ -48,15 +60,14 @@ class ContentManagementModel extends CI_Model {
 	{
 		return $this->db->query("SELECT id,nama AS text FROM content_type_template")->result();
 	}
-	public function getTemplateData($id,string $target=null)
+	public function getTemplateData($id, string $target = null)
 	{
-		if($target===null) {
-			return $this->db->query('SELECT JSON_UNQUOTE(JSON_EXTRACT(template, "$.sms")) AS sms,JSON_UNQUOTE(JSON_EXTRACT(template, "$.whatsapp")) AS whatsapp,JSON_UNQUOTE(JSON_EXTRACT(template, "$.email")) AS email,JSON_UNQUOTE(JSON_EXTRACT(template, "$.landingpage")) AS landingpage FROM content_type_template WHERE id = '.$id.';')->result()[0];
+		if ($target === null) {
+			return $this->db->query('SELECT JSON_UNQUOTE(JSON_EXTRACT(template, "$.sms")) AS sms,JSON_UNQUOTE(JSON_EXTRACT(template, "$.whatsapp")) AS whatsapp,JSON_UNQUOTE(JSON_EXTRACT(template, "$.email")) AS email,JSON_UNQUOTE(JSON_EXTRACT(template, "$.landingpage")) AS landingpage FROM content_type_template WHERE id = ' . $id . ';')->result()[0];
 		} else {
-			return $this->db->query('SELECT JSON_UNQUOTE(JSON_EXTRACT(template, "$.'.$target.'")) AS '.$target.' FROM content_type_template WHERE id = '.$id.';')->result()[0];
+			return $this->db->query('SELECT JSON_UNQUOTE(JSON_EXTRACT(template, "$.' . $target . '")) AS ' . $target . ' FROM content_type_template WHERE id = ' . $id . ';')->result()[0];
 		}
 	}
-
 }
 
 /* End of file contentManagementModel.php */
